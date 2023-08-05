04d5de3ca23542115704e0337263266b6eaea4ab
✳ Adolf Commerce <128338681+adolf-commerce@users.noreply.github.com>
2023-08-05 18:17:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
 let proxyIP = '128.14.140.254';
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables
 let nodeId = ''; // 1
